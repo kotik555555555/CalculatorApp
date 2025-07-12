fd6a4999f9044d72f4256f1902e71205b6e0fabb
kotik555555555 <kotikmurcik11@gmail.com>
2025-07-12 18:27:32 +0300

    Виклик методів калькулятора

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -15,18 +15,17 @@ int multiply(int a, int b) {
 
 double divide(int a, int b) {
     if (b == 0) {
-        cout << "Ділення на нуль!" << endl;
+        cout << "Divide by zero!" << endl;
         return 0;
     }
     return (double)a / b;
 }
 
 int main() {
-    cout << "Калькулятор функцій" << endl;
+    cout << "Functional calculator:" << endl;
     cout << "3 + 2 = " << add(3, 2) << endl;
     cout << "5 - 1 = " << subtract(5, 1) << endl;
     cout << "4 * 2 = " << multiply(4, 2) << endl;
     cout << "10 / 2 = " << divide(10, 2) << endl;
     return 0;
-    }
 }
